6efbabefcff2cd5f7fd0cfe552e1cc4080abe706
William Pannell <lpann5@gmail.com>
2019-10-02 12:07:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ end
 
 def using_pop(array)
   array=["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  array.pop()
+  array.pop(-1)
 end
 
 def pop_with_args(array)
